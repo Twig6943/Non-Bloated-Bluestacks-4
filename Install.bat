8e1e3b9f49bdad242752951c67ad2339d1f9d053
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-05-12 23:56:00 +0300

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,10 +1,8 @@
 cd C:\Users\%USERNAME%\Downloads\
 curl https://cdn3.bluestacks.com/downloads/windows/bgp64/4.150.13.4102/07815e91b9bb304499d2051885e5d9e2/x64/BlueStacks-Installer_4.150.13.4102_amd64_native.exe -o BluestacksInstaller.exe
-curl https://raw.githubusercontent.com/Twig6943/Non-Bloated-Bluestacks/main/readme.txt -o readme.txt
 echo CLOSE THE INSTALLER WHEN ITS DONE DO NOT PRESS LAUNCH
 echo CLOSE THE INSTALLER WHEN ITS DONE DO NOT PRESS LAUNCH
 echo CLOSE THE INSTALLER WHEN ITS DONE DO NOT PRESS LAUNCH
-START C:\Users\%USERNAME%\Downloads\readme.txt
 C:\Users\%USERNAME%\Downloads\BluestacksInstaller.exe
 @NetSh AdvFirewall Set AllProfiles State On
 :: Variables
