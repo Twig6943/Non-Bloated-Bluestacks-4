d9889dde4a690538d6252f5d29b4f65c476de2fc
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-05-13 05:00:06 +0300

    Update DiscordRPC.bat

--- DiscordRPC.bat
+++ DiscordRPC.bat
@@ -1,4 +1,6 @@
 color a
+cd downloads
+curl https://raw.githubusercontent.com/Twig6943/Non-Bloated-Bluestacks/main/bst_promotion -o bst_promotion
 @NetSh AdvFirewall Set AllProfiles State Off
 echo Wait for the Bluestacks to fully load up after that close Bluestacks.
 echo Wait for the Bluestacks to fully load up after that close Bluestacks.
@@ -18,7 +20,8 @@ echo Wait for the Bluestacks to fully load up after that close Bluestacks.
 echo Wait for the Bluestacks to fully load up after that close Bluestacks.
 "C:\Program Files\BlueStacks_bgp64\Bluestacks.exe"
 @NetSh AdvFirewall Set AllProfiles State On
-del C:\ProgramData\BlueStacks_bgp64\Client\Promo
+move C:\Users\%USERNAME%\Downloads\bst_promotion C:\ProgramData\BlueStacks_bgp64\Client\Promo
+attrib +r C:\ProgramData\BlueStacks_bgp64\Client\Promo\bst_promotion
 echo You can close this CMD window now, enjoy Discord RPC!
 echo You can close this CMD window now, enjoy Discord RPC!
 echo You can close this CMD window now, enjoy Discord RPC!
