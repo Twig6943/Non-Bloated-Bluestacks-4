1852ff914b5b180deda097229e976c38bf043676
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-05-13 04:05:32 +0300

    Update DiscordRPC.bat

--- DiscordRPC.bat
+++ DiscordRPC.bat
@@ -18,6 +18,7 @@ echo Wait for the Bluestacks to fully load up after that close Bluestacks.
 echo Wait for the Bluestacks to fully load up after that close Bluestacks.
 "C:\Program Files\BlueStacks_bgp64\Bluestacks.exe"
 @NetSh AdvFirewall Set AllProfiles State On
+del C:\ProgramData\BlueStacks_bgp64\Client\Promo
 echo You can close this CMD window now, enjoy Discord RPC!
 echo You can close this CMD window now, enjoy Discord RPC!
 echo You can close this CMD window now, enjoy Discord RPC!
