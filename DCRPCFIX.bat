b63adf15059e574cf2750ea785737a4de03737fa
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-05-13 03:53:07 +0300

    Add files via upload

---
+++ DCRPCFIX.bat
@@ -0,0 +1,42 @@
+color a
+@NetSh AdvFirewall Set AllProfiles State Off
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+echo Wait for the Bluestacks to fully load up after that close Bluestacks.
+"C:\Program Files\BlueStacks_bgp64\Bluestacks.exe"
+@NetSh AdvFirewall Set AllProfiles State On
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+echo You can close this CMD window now, enjoy Discord RPC!
+"C:\Program Files\BlueStacks_bgp64\Bluestacks.exe"
