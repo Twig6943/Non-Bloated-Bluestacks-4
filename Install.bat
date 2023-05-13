8c35b3821d8ea49cea8bfc637b01f3922a4edb2d
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-05-13 03:40:16 +0300

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,4 +1,3 @@
-@echo off
 color a
 cd C:\Users\%USERNAME%\Downloads\
 curl https://cdn3.bluestacks.com/downloads/windows/bgp64/4.280.4.4002/c0e5b72daaec1a9e063fa25504c24753/x64/BlueStacks-Installer_4.280.4.4002_amd64_native.exe -o BluestacksInstaller.exe
